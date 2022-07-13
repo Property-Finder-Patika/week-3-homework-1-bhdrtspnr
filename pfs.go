1bbbf7afb13e3034645ee623d7a97ff9ceaa82c1
bhdrtspnr <97244264+bhdrtspnr@users.noreply.github.com>
2022-07-13 18:34:09 +0300

    Create pfs.go

---
+++ pfs.go
@@ -0,0 +1,22 @@
+package main
+
+func PrimeFactors(n int) (pfs []int) {
+	for n%2 == 0 {
+		pfs = append(pfs, 2)
+		n = n / 2
+	}
+
+
+	for i := 3; i*i <= n; i = i + 2 {
+		for n%i == 0 {
+			pfs = append(pfs, i)
+			n = n / i
+		}
+	}
+
+	if n > 2 {
+		pfs = append(pfs, n)
+	}
+
+	return
+}
